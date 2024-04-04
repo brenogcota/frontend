d9f81a920c130b86c3d551d16327b9f6797c66ac
Breno Cota <46490801+brenogcota@users.noreply.github.com>
2024-04-04 09:35:58 -0300

    fix: business logic

--- src/utils/calculateCompoundIncome.ts
+++ src/utils/calculateCompoundIncome.ts
@@ -1,11 +1,10 @@
 export const calculateCompoundIncome = (principal: number, tax: number, time: number, monthly: number) => {
     const decimalRate = tax / 12 / 100;
     let amount = principal;
-    const t = (time / 12)
 
     for (let i = 0; i < time; i++) {
-        amount *= Math.pow(1 + decimalRate, t)
-        amount += monthly
+        amount *= (1 + decimalRate);
+        amount += monthly;
     }
     
     return amount;
